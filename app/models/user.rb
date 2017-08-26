6740a490ed4374ab9d5f75d6fa59b43dd8ec4a32
kenneth1870 <kenneth1870@hotmail.com>
2017-08-26 11:17:02 -0600

    add Devise for admin section

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: {
+    user: 0,
+    admin: 1
+  }
+end
