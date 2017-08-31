3b261903b906f273849b812e08550dadc5481233
Lilliam Arce <lilliam@foundationlabs.co>
2017-08-31 13:51:20 -0600

    Changing js redirect

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,10 @@
-function moveToSuscrive(){
-  document.getElementById('inscribirse').scrollIntoView();
-}
+
+$(document).on('click', '#suscribe-btn', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+   }
+ });
