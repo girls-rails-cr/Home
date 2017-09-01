3cfc2740d5822a041c6e712b135ff619185906e5
Lilliam Arce <lilliam@foundationlabs.co>
2017-09-01 10:41:10 -0600

    Validate fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901150109) do
+ActiveRecord::Schema.define(version: 20170901150721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20170901150109) do
     t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "knowledge"
+    t.integer "level_knowledge"
+    t.string "phone_number"
   end
 
   create_table "users", force: :cascade do |t|
