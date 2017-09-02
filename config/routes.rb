d258ec3ff254dec56eadbd8963bbb2f7ad2e86f1
kennethrosales <kenneth.rosales@4geeks.io>
2017-09-01 22:20:25 -0600

    add destroy user session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :subscribers , only: [:create]
 
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
