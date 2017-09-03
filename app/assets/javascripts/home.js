1518409904c7f7761d98dd31915ebedb860115ae
kennethrosales <kenneth.rosales@4geeks.io>
2017-09-02 21:47:51 -0600

    add timetable for event

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 
-$(document).on('click', '#suscribe-btn', function(event) {
+$(document).on('click', '.suscribe-btn', function(event) {
     var target = $(this.getAttribute('href'));
     if( target.length ) {
       event.preventDefault();
