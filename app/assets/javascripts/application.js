489ee5953f2772158b3ac9f15b79125461b83c22
Lilliam Arce <lilliamarce@Lilliams-MacBook-Pro.local>
2017-12-23 18:20:21 -0600

    Adding partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,9 @@
 //
 //= require jquery/dist/jquery
 //= require jquery-ujs/src/rails
-//= require foundation-sites/dist/js/foundation
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
 //= require turbolinks
 
