73e34e1a6501902d8380d84c83c4805d9da9e035
greenscript <diegocanocamacho23@gmail.com>
2018-01-17 00:02:26 -0600

    routes, about, contact and sponsors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,16 @@ class PagesController < ApplicationController
   def index
   	@subscribers = Subscriber.all
   end
+  def contact
+
+  end
+  def faq
+
+  end
+  def about
+
+  end
+  def sponsors
+
+  end
 end
