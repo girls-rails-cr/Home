250a899dbf4f86d0c05539bdb5600c78f1ead9d7
kenneth1870 <kenneth1870@hotmail.com>
2018-01-29 22:57:30 -0600

    update gems

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.staging?
     config.fog_credentials = {
       provider:              'AWS',                                   # required
-      aws_access_key_id:     Rails.configuration.aws['aws_access_key_id'],     # required
-      aws_secret_access_key: Rails.configuration.aws['aws_secret_access_key'], # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],     # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] # required
       #region:               'eu-west-1'                              # optional, defaults to 'us-east-1'
       #host:                 's3.example.com',                        # optional, defaults to nil
       #endpoint:             'https://s3.example.com:8080'            # optional, defaults to nil
@@ -21,7 +21,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                                 # required
     config.storage = :fog
 
-    config.fog_directory   = Rails.configuration.aws['aws_bucket_name']        # required
+    config.fog_directory   = ENV['AWS_BUCKET_NAME']       # required
     #config.fog_host       = 'https://assets.example.com'             # optional, defaults to nil
     #config.fog_public     = false                                    # optional, defaults to true
     config.fog_attributes  = {'Cache-Control'=>'max-age=315576000'}   # optional, defaults to {}
